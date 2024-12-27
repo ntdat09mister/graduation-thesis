79563dc6c3e6de410d4a798859ff9309a6efeee3
ntdat09mister <104963406+ntdat09mister@users.noreply.github.com>
2024-12-27 09:45:54 +0700

    Update .sql

--- danny-store/.sql
+++ danny-store/.sql
@@ -13,7 +13,7 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+create database danny_store_db character set utf8mb4 COLLATE utf8mb4_general_ci;
 -- Dumping structure for table danny-store-db.cart
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE IF NOT EXISTS `cart` (
